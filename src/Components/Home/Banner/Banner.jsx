82a7cfa80aaddaab7e7559a04a5d19ebf110fffc
naimur8606 <119111473+naimur8606@users.noreply.github.com>
2023-09-25 21:05:48 +0600

    search feature added

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -1,12 +1,12 @@
-const Banner = () => {
+const Banner = ({catchInput}) => {
     return (
         <div className='h-[40vh] lg:h-[75vh] rounded p-5 bg-blend-overlay bg-white bg-opacity-90 bg-cover bg-[url("https://i.ibb.co/Bsjk5R8/istockphoto-1271755758-170667a.jpg")] flex justify-center items-center'>
             <div className="text-center">
                 <h1 className="text-2xl md:text-5xl font-bold">I Grow By Helping People In Need</h1>
-                <div className="flex md:w-1/2 mt-5 mx-auto border">
-                    <input className="w-2/3 rounded-l-lg pl-2" type="text" placeholder="Search here..."/>
-                    <p className="text-white text-xl rounded-r-lg py-2.5 px-4 bg-red-500">Search</p>
-                </div>
+                <form onSubmit={catchInput} className="flex md:w-1/2 mt-5 mx-auto border">
+                    <input className="w-2/3 rounded-l-lg pl-2" type="text" name="search" placeholder="Search here..."/>
+                    <button type="submit" value='submit' className="text-white text-xl rounded-r-lg py-2.5 px-4 bg-red-500">Search</button>
+                </form>
             </div>
         </div>
     );
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,27 @@
 import { useLoaderData } from "react-router-dom";
 import Banner from "./Banner/Banner";
 import DonationItems from "./DonationItems/DonationItems";
+import { useState, useEffect } from "react";
 
 const Home = () => {
-    const DonationAllData = useLoaderData()
+    const [DonationAllData, setDonationAllData] = useState([]);
+    const catchDonationAllData = useLoaderData();
+
+    useEffect(() => {
+        // Set the DonationAllData state after catching the data
+        setDonationAllData([...catchDonationAllData]);
+    }, [catchDonationAllData]); // Run this effect whenever catchDonationAllData changes
+
+    const catchInput = (e) => {
+        e.preventDefault();
+        const searchValue = e.target.search.value;
+        const searchDonation =catchDonationAllData.filter(item => item.category_name == searchValue)
+        setDonationAllData([...searchDonation])
+    };
+
     return (
         <div>
-            <Banner></Banner>
+            <Banner catchInput={catchInput}></Banner>
             <DonationItems DonationAllData={DonationAllData}></DonationItems>
         </div>
     );
